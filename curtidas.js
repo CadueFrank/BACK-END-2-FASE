063ae00cdfd61a8251b75865a9d64e5b0cb61804
CadueFrank <carlos.machado14@escola.pr.gov.br>
2026-06-29 13:46:53 +0000

    versao2

--- curtidas.js
+++ curtidas.js
@@ -17,3 +17,17 @@ if(!post.publicado ) {
 
 post.metricas.curtidas++;
 console.log(`Post de @${post.autor} agora tem ${post.mestricas.curtidas} curtidas.`);
+
+function descurtir(feed, id) {
+    const post = feed[0].id === id ? feed[0] :
+            feed[1].id === ? feed[1]: feed[2];
+}
+
+
+if(post.metricas.curtidas === 0) {
+    console.log("Este post já está com 0 curtidas.");
+    return;
+}
+
+post.metricas.curtidas++;
+console.log(`Curtida removida ♡💔💔♡. Total ${post.metricas.curtidas}`)
