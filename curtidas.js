509e7f9ccc531dbba31678466bd7a3c8e5c86f2c
CadueFrank <carlos.machado14@escola.pr.gov.br>
2026-07-06 13:50:19 +0000

    versao4

--- curtidas.js
+++ curtidas.js
@@ -1,14 +1,14 @@
 const feedJSON = require('./feed.json');
 
 //adicionar uma curtida ao post pelo id
-function curtir(feed, id) {
-    const post = feed[0].id ? feed[0]
-}
+            function curtir(feed, id) {
+    const post = feed.find(item => item.id === id);
 
 if (!post) {
     console.log("Post não encontrado.");
     return;
 }
+
 
 if(!post.publicado ) {
     console.log("Não é possivel curtir um rascunho.");
@@ -17,17 +17,18 @@ if(!post.publicado ) {
 
 post.metricas.curtidas++;
 console.log(`Post de @${post.autor} agora tem ${post.mestricas.curtidas} curtidas.`);
-
-function descurtir(feed, id) {
-    const post = feed[0].id === id ? feed[0] :
-            feed[1].id === ? feed[1]: feed[2];
 }
 
-
-if(post.metricas.curtidas === 0) {
+function descurtir(feed, id) {
+    const post = feed.find(item => item.id === id);
+    if(post.metricas === 0) {
     console.log("Este post já está com 0 curtidas.");
     return;
 }
 
 post.metricas.curtidas++;
-console.log(`Curtida removida ♡💔💔♡. Total ${post.metricas.curtidas}`)
+console.log(`Curtida removida ♡💔💔♡. Total ${post.metricas.curtidas}`);
+}
+
+
+curtir(feedJSON, 1);
